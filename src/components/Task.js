fba914b11c0a0a54b8251eb9d0f0ee2dd101a9d4
izabelka <i.konstanciak@gmail.com>
2018-09-30 16:39:06 -0700

    Add a button for hiding tasks

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,20 +3,41 @@ import styled from 'styled-components';
 
 class Task extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      hidden: false,
+    };
+  }
+
+  onHideClick = () => {
+    this.setState({
+      hidden: true,
+    })
+  }
+
   render() {
     return (
-      <TaskWrapper>
+      <TaskWrapper
+        hidden={this.state.hidden}>
         <TaskName>
           {this.props.task.name}
         </TaskName>
+        <Button
+          onClick={this.onHideClick}>
+          {'Hide'}
+        </Button>
       </TaskWrapper>
     );
   }
 }
 
 const TaskWrapper = styled.li`
+  display: ${({ hidden }) => hidden ? 'none' : 'flex'};
+  align-items: center;
+  justify-content: space-between;
   font-size: 1.1em;
-  padding-bottom: 14px;
+  padding-bottom: 10px;
   border-bottom: 1px solid #c6c5cf;
   &:not(:last-of-type) {
     margin-bottom: 34px;
@@ -24,6 +45,19 @@ const TaskWrapper = styled.li`
 `;
 
 const TaskName = styled.span`
+`;
 
+const Button = styled.span`
+  background: #14aaf5;
+  color: #fff;
+  height: 34px;
+  line-height: 34px;
+  padding: 0 9px;
+  border-radius: 4px;
+  cursor: pointer;
+
+  &:hover {
+    background: #4186e0;
+  }
 `;
 export default Task;
